54a48c03d31da89817edea3392c9c63a4d2a8ce4
THE-VR7 <vineetrathor57@gmail.com>
2019-11-13 01:23:18 +0530

    krishna sudarshan

--- app.js
+++ app.js
@@ -8,13 +8,20 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 const express = require('express');
 const app = express();
-
+const http = require('http').Server(app);
 
 
 mongoose.connect("mongodb://localhost/food_app", function (err, db) {
     if (!(err)) console.log("you are connected to mongodb");
     if(err) console.log('not connected to mongodb');
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("express-session")({
+    secret: "Rusty is the best og in the world",
+    resave: false,
+    saveUninitialized: false
+}));
 
 var db=mongoose.connection; 
 db.on('error', console.log.bind(console, "connection error")); 
@@ -29,44 +36,37 @@ app.set('view engine', 'ejs');
 //
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 // 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.post('/', function(req,res){ 
-    var Name = req.body.Name; 
-    var email =req.body.email; 
-    var phonenumber = req.body.phonenumber; 
-    var text =req.body.text;
-    var data = { 
-        "Name": Name, 
-        "email":email, 
-        "phonenumber":phonenumber, 
-        "text":text 
-    } 
-db.collection('details').insert(data,function(err, collection){ 
-        if (err) throw err; 
-        console.log("Record inserted Successfully");
+// app.post('/', function(req,res){ 
+//     var Name = req.body.Name; 
+//     var email =req.body.email; 
+//     var phonenumber = req.body.phonenumber; 
+//     var text =req.body.text;
+//     var data = { 
+//         "Name": Name, 
+//         "email":email, 
+//         "phonenumber":phonenumber, 
+//         "text":text 
+//     } 
+// db.collection('details').insert(data,function(err, collection){ 
+//         if (err) throw err; 
+//         console.log("Record inserted Successfully");
               
-    }); 
+//     }); 
           
-    return res.redirect('/'); 
-}) 
+//     return res.redirect('/'); 
+// }) 
 
 app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get("/login", function (req, res) {
-    res.render("login");
-});
 
-app.get("/register", function (req, res) {
-    res.render("/")
-    // document.getElementById().style.display='none',document.getElementById('id02').style.display='block'
-});
 //handling user sign up
 app.post("/register", function (req, res) {
     User.register(new User({ username: req.body.username,email: req.body.email,name: req.body.name }),req.body.password, function (err, user) {
@@ -75,23 +75,30 @@ app.post("/register", function (req, res) {
             return res.render('/register');
         } //user stragety
         passport.authenticate("local")(req, res, function () {
-            document.getElementById('id01').style.display='block'
-            document.getElementById('id02').style.display='none'
-            res.redirect("/"); //once the user sign up
+            console.log(req.body.name)
+            console.log(req.body.username)
+            console.log(req.body.email)
+            res.redirect("/in"); //once the user sign up
         });
     });
 });
 
 // Login Routes
 
-app.get("/login", function (req, res) {
-    res.render("login.ejs");
-})
+
+app.get("/in",isLoggedIn, function (req, res) {
+    res.render("in",{name: req.user.username});
+    
+    console.log(req.user.username);
+});
+app.get("/",isLoggedIn, function (req, res) {
+    res.render("in",{name: req.user.username});
+});
 
 // middleware
 app.post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login"
+    successRedirect: "/in",
+    failureRedirect: "/"
 }), function (req, res) {
     res.send("User is " + req.user.id);
 });
@@ -101,18 +108,17 @@ app.get("/logout", function (req, res) {
     res.redirect("/");
 });
 
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    res.redirect("/login");
+    res.redirect("/in");
 }
 
+const server = http.listen(8081, function () {
+    console.log('listening on *:8081');
+});
 
-app.listen(3000, function(req, res) {
-    console.log("Server started at port 3000.....")
-}
-)
-
-module.exports=User;
-module.exports=mongoose;
+// module.exports=User;
+// module.exports=mongoose;
